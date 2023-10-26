6e125345ce4759224c26033a7f0217866f6c967c
MD FAISAL <faisalcse1@gmail.com>
2023-10-27 02:28:55 +0600

    test done..

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+using HtmlToPdfMaster;
+//For HTML context
+//string htmlContenet = File.ReadAllText("C:\\Users\\MD FAISAL\\Desktop\\HtmlFile\\demohtml.txt");
+//var pdfFile= HtmlConverter.FromHtmlString(htmlContenet, 220, 335);
+
+//For URL
+var pdfFile = HtmlConverter.FromUrl("https://www.w3schools.com/html/", 220,335);
+
+string fileName=Guid.NewGuid().ToString()+".pdf";
+File.WriteAllBytes("C:\\Users\\MD FAISAL\\Desktop\\HtmlFile\\"+fileName, pdfFile);
 Console.WriteLine("Hello, World!");
